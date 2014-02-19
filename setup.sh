592217ca681b3924d34708d9053406140a920589
ssl-umd <ssl.mxsuit@gmail.com>
2014-02-19 17:09:20 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Setup script version 1.0
 SERENVERSION="seren-0.0.17"
 FILENAME=$SERENVERSION".tar.gz"
 CONFERENCEIP=192.168.1.20
@@ -33,13 +34,21 @@ make install
 cd ..
 
 echo "creating scripts..."
+
 echo "#!/bin/bash" > callIP
 echo "echo \"connecting to voice conference on \"\$1" >> callIP
 echo "/home/pi/suit/"$SERENVERSION"/seren -C0 -S -N -c \$1 -n pi -d plug:front:Set > callIP.out" >> callIP
+
+echo "#!/bin/bash" > startConference
+echo "echo \"initializing voice conference" >> startConference
+echo "/home/pi/suit/"$SERENVERSION"/seren -C0 -S -N -n pi -a -d plug:front:Set > startConference.out" >> startConference
+
 echo "#!/bin/bash" > initCall
 echo "/home/pi/suit/startVoice" >> initCall
+
 echo "#!/bin/bash" > joinCall
 echo "/home/pi/suit/callIP $CONFERENCEIP" >> joinCall
+
 echo "#!/bin/bash" > startVoice
 echo "echo \"running voice conference initialization script\"" >> startVoice
 echo "/home/pi/suit/joinCall" >> startVoice
@@ -49,6 +58,7 @@ chmod 755 initCall
 chmod 755 startVoice
 chmod 755 joinCall
 chmod 755 callIP
+chmod 755 startConference
 
 echo "setting up init.d..."
 cp initCall /etc/init.d
